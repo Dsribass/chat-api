370369ced95b9e5b5c287f9ebd2184372c26d56c
Daniel Ribas <ds_ribas@hotmail.com>
2024-04-25 17:47:55 -0300

    feat: change model name

--- src/models/message.ts
+++ src/models/message.ts
@@ -6,21 +6,18 @@ export class Message {
     channelId: string;
     content: string;
     senderId: string;
-    receiverId: string;
     timestamp?: number;
   }) {
     this.id = props.id ?? randomUUID();
     this.content = props.content;
     this.channelId = props.channelId;
     this.senderId = props.senderId;
-    this.receiverId = props.receiverId;
     this.timestamp = props.timestamp ?? Date.now();
   }
 
   readonly id: string;
   readonly channelId: string;
   readonly senderId: string;
-  readonly receiverId: string;
   readonly content: string;
   readonly timestamp: number;
 }
--- src/schemas/create-channel-schema.ts
+++ src/schemas/create-channel-schema.ts
@@ -4,7 +4,7 @@ import userSchemaProps from "./user-schema-props";
 
 export default {
   body: z.object({
-    type: z.enum(["group", "private"]),
+    type: z.enum(["group", "direct"]),
     name: z.string().min(1).optional().nullable(),
     members: z.array(userSchemaProps.email).min(2),
   }),
