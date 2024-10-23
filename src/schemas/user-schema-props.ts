2b808b09a379f18237a88cbd3a89eda3e3e5610f
Daniel Ribas <ds_ribas@hotmail.com>
2024-10-23 17:04:20 -0300

    refactor: improve password validation in user schema

--- src/schemas/user-schema-props.ts
+++ src/schemas/user-schema-props.ts
@@ -3,5 +3,8 @@ import { z } from "zod";
 export default {
   name: z.string().min(3).max(255),
   email: z.string().email(),
-  password: z.string().min(8).max(255),
+  password: z.string()
+    .min(8)
+    .max(255)
+    .regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/, { message: "Password must contain at least one letter and one number" }),
 };
